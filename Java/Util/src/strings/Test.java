e15b12bff11c398a83f66c24ae69e53944c55d05
Fauno Guazina <prof.guazina@gmail.com>
2020-12-17 12:07:02 +0100

    testing restructuring to suit the new implementation

--- Java/Util/src/strings/Test.java
+++ Java/Util/src/strings/Test.java
@@ -5,22 +5,45 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		String [] exceptions = {"<<teSt<<", "teSt_tEst"};
+		String [] exceptions = {"<<teSt<<", "teSt_tEst", "the"};
 		
 		String strTest = "   TEST tEsT, (TEST.TEST), <<teSt<<, teSt_tEst,   mc.beTh, hU-zIn-xun,   d'marcO'aires, tes!te, tESt.. tEsT!! TEST??? 'test' `test` 'test' \"test\"   ";
+		String strTest2 = "the other words will be the words";
 		
-		System.out.println(TitleCase.of(strTest));
-		System.out.println(TitleCase.of(strTest, exceptions));
-		System.out.println(TitleCase.of(strTest, true, exceptions));
-		System.out.println(TitleCase.of(strTest, true, true, "<<teSt<<", "teSt_tEst"));
+		System.out.println("STRINGS TEST:");
+		System.out.println(strTest);
+		System.out.println(strTest2);
 		
-		/* OUTPUT SHOULD BE:
- 		Test Test, (Test.Test), <<Test<<, Test_Test, Mc.Beth, Hu-Zin-Xun, D'Marco'Aires, Tes!Te, Test.. Test!! Test??? 'Test' `Test` 'Test' "Test"
-		Test Test, (Test.Test), <<Test<<, Test_Test, Mc.Beth, Hu-Zin-Xun, D'Marco'Aires, Tes!Te, Test.. Test!! Test??? 'Test' `Test` 'Test' "Test"
-		Test Test, (Test.Test), <<Test<<, teSt_tEst, Mc.Beth, Hu-Zin-Xun, D'Marco'Aires, Tes!Te, Test.. Test!! Test??? 'Test' `Test` 'Test' "Test"
-		TEST Test, (TEST.TEST), <<teSt<<, teSt_tEst, Mc.Beth, Hu-Zin-Xun, D'Marco'Aires, Tes!Te, Test.. Test!! TEST??? 'Test' `Test` 'Test' "Test"
-		 */
 		
+		System.out.println("\n\nSTATIC METHOD TESTING");
+		System.out.println(TitleCase.all(strTest));
+		System.out.println(TitleCase.withExceptions(strTest, exceptions));
+		System.out.println(TitleCase.withCAPITAL(strTest, "<<teSt<<", "teSt_tEst"));
+		System.out.println(TitleCase.withExceptions(strTest2, exceptions));
+		
+		System.out.println("\nINSTANCE METHOD TESTING: ");
+		TitleCase to = new TitleCase(exceptions);
+		System.out.println(to.titleCase(strTest));
+		System.out.println(to.titleCAPITAL(strTest));
+		System.out.println(to.titleCase(strTest2));
+		
+		
+//		OUTPUT SHOULD BE:
+//			STRINGS TEST:
+//			   TEST tEsT, (TEST.TEST), <<teSt<<, teSt_tEst,   mc.beTh, hU-zIn-xun,   d'marcO'aires, tes!te, tESt.. tEsT!! TEST??? 'test' `test` 'test' "test"   
+//			the other words will be the words
+//
+//
+//			STATIC METHOD TESTING
+//			Test Test, (Test.Test), <<Test<<, Test_Test, Mc.Beth, Hu-Zin-Xun, D'Marco'Aires, Tes!Te, Test.. Test!! Test??? 'Test' `Test` 'Test' "Test"
+//			Test Test, (Test.Test), <<teSt<<, teSt_tEst, Mc.Beth, Hu-Zin-Xun, D'Marco'Aires, Tes!Te, Test.. Test!! Test??? 'Test' `Test` 'Test' "Test"
+//			TEST Test, (TEST.TEST), <<teSt<<, teSt_tEst, Mc.Beth, Hu-Zin-Xun, D'Marco'Aires, Tes!Te, Test.. Test!! TEST??? 'Test' `Test` 'Test' "Test"
+//			The Other Words Will Be the Words
+//
+//			INSTANCE METHOD TESTING: 
+//			Test Test, (Test.Test), <<teSt<<, teSt_tEst, Mc.Beth, Hu-Zin-Xun, D'Marco'Aires, Tes!Te, Test.. Test!! Test??? 'Test' `Test` 'Test' "Test"
+//			TEST Test, (TEST.TEST), <<teSt<<, teSt_tEst, Mc.Beth, Hu-Zin-Xun, D'Marco'Aires, Tes!Te, Test.. Test!! TEST??? 'Test' `Test` 'Test' "Test"
+//			The Other Words Will Be the Words		
 	}
 	
 }
